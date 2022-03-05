3751c9b4202e8d8c4f74ebc783c3a08506912acf
dmp3872@g.rit.edu <dmp3872@g.rit.edu>
2022-03-05 17:27:03 -0500

    fixed reminder format

--- src/components/reminders/Reminders.jsx
+++ src/components/reminders/Reminders.jsx
@@ -9,15 +9,15 @@ export default function Reminders() {
     const number=e.target.number.value;
     const reminder =e.target.reminder.value;
     const time = e.target.time.value;
-    const last=e.target.days.value;
+    const days =e.target.days.value;
   }
   return (
     <div className="reminders" id="reminders">
       <h1>Set a new Reminder</h1>
       <form onSubmit={handleSumbit}>
-        <input type="text" name="number" placeholder="phone number"/>
+        <input type="text" name="number" placeholder="Phone Number"/>
         <textarea name="reminder" placeholder="Set Reminder..."></textarea>
-        <input type="text" className="time" placeholder="time"/>
+        <input type="text" className="time" placeholder="Time"/>
         <input type="text" name="days" placeholder="# of days per week"/>
         <button type= "submit">Add Reminder</button>
         {message && <span>Reminder Added</span>}
