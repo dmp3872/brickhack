ef67d3ad16da694342413358ba64652d0dcf085b
dmp3872@g.rit.edu <dmp3872@g.rit.edu>
2022-03-05 17:20:19 -0500

    Reminder Feature added

--- src/components/reminders/Reminders.jsx
+++ src/components/reminders/Reminders.jsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import "./reminders.scss"
 
 export default function Reminders() {
+  const[message,setMessage] = useState(false)
+  const handleSumbit =(e)=>{
+    e.preventDefault();
+    setMessage(true)
+    const number=e.target.number.value;
+    const reminder =e.target.reminder.value;
+    const time = e.target.time.value;
+    const last=e.target.days.value;
+  }
   return (
     <div className="reminders" id="reminders">
-
-    </div>
+      <h1>Set a new Reminder</h1>
+      <form onSubmit={handleSumbit}>
+        <input type="text" name="number" placeholder="phone number"/>
+        <textarea name="reminder" placeholder="Set Reminder..."></textarea>
+        <input type="text" className="time" placeholder="time"/>
+        <input type="text" name="days" placeholder="# of days per week"/>
+        <button type= "submit">Add Reminder</button>
+        {message && <span>Reminder Added</span>}
+        </form>
+      </div>
   )
+
 }
