557aede7a1bbf2eb66dc736a4f158897be40b39b
Kellen Bell <kdb9520@rit.edu>
2022-03-06 06:47:27 -0500

    listing recommendation

--- src/components/recommendations/Recommendations.jsx
+++ src/components/recommendations/Recommendations.jsx
@@ -3,8 +3,18 @@ import "./recommendations.scss"
 export default function Recommendations() {
   return (
     <div className="recommendations">
-        <h1>Recommended Reminders</h1>
-        
-        </div>
+      <body>
+        <h1><b>Recommended Reminders</b></h1>
+        <p>Set a Reminder to:
+            <ul>
+              <li>Workout</li>
+              <li>Go to sleep on time</li>
+              <li>Do your homework</li>
+              <li>Drink water</li>
+              <li>Schedule an appointment</li>
+            </ul>
+        </p> 
+      </body>   
+    </div>
   )
 }
