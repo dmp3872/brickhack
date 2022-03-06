3802a32a0bb07a399a96163d6dd37eef61df1ad9
Ethan Vuong <ecv9229@rit.edu>
2022-03-05 19:20:14 -0500

    Contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,25 @@
 import "./contact.scss"
+import React, { useState } from "react";
 
 export default function Contact() {
+const [message,setMessage] = useState(false)
+
+
+
   return (
     <div className="contact" id="contact">
-
+          <div className="left">
+          <h2>Contact.</h2>  
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Email"/>
+            <textarea placeholder="Message"></textarea>
+            <button>Submit</button>
+            {message && <span>Your message has been received!</span>}
+          </form>
+        </div>
+        <div className="right">
+            
+        </div>
     </div>
   )
 }
