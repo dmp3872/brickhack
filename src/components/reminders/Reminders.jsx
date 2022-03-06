103af7d012fee29b9eaff62de166904af2e5e91d
Kellen Bell <kdb9520@rit.edu>
2022-03-05 21:00:49 -0500

    testing if reminders works

--- src/components/reminders/Reminders.jsx
+++ src/components/reminders/Reminders.jsx
@@ -1,7 +1,20 @@
+import { state } from "express";
 import { useState } from "react";
 import "./reminders.scss"
 
 export default function Reminders() {
+  state = {
+    text: {
+      recipient: '',
+      textmessage: ''
+    }
+  }
+  function sendText() {
+    const { text } = Reminders.state;
+    //pass text message GET variables via query string
+    fetch(`http://127.0.0.1:4000/send-text?recipient=${text.recipient}&textmessage=${text.textmessage}`)
+      .catch(err => console.error(err));
+  }
   const[message,setMessage] = useState(false)
   const handleSumbit =(e)=>{
     e.preventDefault();
@@ -10,6 +23,8 @@ export default function Reminders() {
     const reminder= e.target.reminder.value;
     const time = e.target.time.value;
     const days = e.target.days.value;
+    Reminders.state = {number, reminder};
+    sendText();
   }
   return (
     <div className="reminders" id="reminders">
