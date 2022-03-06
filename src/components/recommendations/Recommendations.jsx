511877e23d276c3fdd294ed1afa5c1583f2ccdfe
dmp3872@g.rit.edu <dmp3872@g.rit.edu>
2022-03-06 06:49:33 -0500

    reminders added

--- src/components/recommendations/Recommendations.jsx
+++ src/components/recommendations/Recommendations.jsx
@@ -2,8 +2,19 @@ import "./recommendations.scss"
 
 export default function Recommendations() {
   return (
-    <div className="recommended" id="recommended">
-        <h1>Recommended Reminders</h1>
-        </div>
+    <div className="recommendations">
+      <body>
+        <h1><b>Recommended Reminders</b></h1>
+        <p>Set a Reminder to:
+            <ul>
+              <li>Workout</li>
+              <li>Go to sleep on time</li>
+              <li>Do your homework</li>
+              <li>Drink water</li>
+              <li>Schedule an appointment</li>
+            </ul>
+        </p> 
+      </body>
+    </div>
   )
 }
