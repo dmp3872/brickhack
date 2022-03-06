f1072408a1191ac002cc4200014fb912e43232ed
dmp3872@g.rit.edu <dmp3872@g.rit.edu>
2022-03-06 07:23:41 -0500

    fixed reminder list

--- src/components/recommendations/Recommendations.jsx
+++ src/components/recommendations/Recommendations.jsx
@@ -3,17 +3,15 @@ import "./recommendations.scss"
 export default function Recommendations() {
   return (
     <div className="recommended" id="recommended">
-      <body>
         <h1><b>Recommended Reminders</b></h1>
         <h2>Set a Reminder to:</h2>
         <ul>
           <li>Workout</li>
-              <li>Go to sleep on time<br/></li>
-              <li>Do your homework<br/></li>
-              <li>Drink water<br/></li>
-              <li>Schedule an appointment<br/></li>
+              <li>Go to sleep on time</li>
+              <li>Do your homework</li>
+              <li>Drink water</li>
+              <li>Schedule an appointment</li>
             </ul>
-      </body>
       <a href="#contact">
            <img src="assets/down3.png" alt="" />
            </a>
