edeb0dcb86adb5e9e3144305cc3f44ce93485241
dmp3872@g.rit.edu <dmp3872@g.rit.edu>
2022-03-05 19:36:12 -0500

    fixed contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,10 @@ import React, { useState } from "react";
 export default function Contact() {
 const [message,setMessage] = useState(false)
 
-
+const handleSubmit = (e) => {
+  e.preventDefault();
+  setMessage(true);
+};
 
   return (
     <div className="contact" id="contact">
